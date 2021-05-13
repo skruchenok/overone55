4aea10e89ebfd9e7c3e8614925a4e0198289ff70
boss <botviinikov@bk.ru>
2021-05-13 20:57:52 +0300

    added comp

---
+++ hard_task.py
@@ -0,0 +1,15 @@
+def comp(num1, num2):
+    if (type(num1) == int or type(num1) == float) and (type(num2) == int or type(num2) == float):
+        if num1 > num2:
+            return num1
+        elif num1 < num2:
+            return num2
+        else:
+            return num1
+    else:
+        raise ValueError("Некоректные числа для сравнения")
+
+try:
+    print(comp(-9.9,-1.9))
+except ValueError as e:
+    print(e)
